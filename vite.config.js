a6433be88189c9254c70704d5ddb1814a30aae90
BallOne69noway <diskord.gg_rus@mail.ru>
2026-03-09 20:55:43 +0500

    Add: Vite config for GitHub Pages deployment

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/rust-vortex/', // Это название твоего репозитория на GitHub
+})
