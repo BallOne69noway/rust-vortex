cc3730e9dba06aa94559ccafcc02fc90aeacb026
BallOne69noway <diskord.gg_rus@mail.ru>
2026-03-11 12:51:26 +0500

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/react-web' // или просто react()
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rust-vortex/', // Это название твоего репозитория на GitHub
+  base: '/rust-vortex/', // Добавь эту строку!
 })
