03449746921817ddd17bd7dcb984a6f5b80d6d3c
BallOne69noway <diskord.gg_rus@mail.ru>
2026-03-11 12:57:15 +0500

    Fix vite config plugin and base path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/react-refresh' 
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/rust-vortex/', 
+  base: '/rust-vortex/',
 })
